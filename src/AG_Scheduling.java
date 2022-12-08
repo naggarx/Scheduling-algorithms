422e3ba040db1b48f74c54569add710e6ad9d6af
naggarx <72861717+naggarx@users.noreply.github.com>
2022-12-08 17:35:11 +0200

    Update AG_Scheduling.java

--- src/AG_Scheduling.java
+++ src/AG_Scheduling.java
@@ -8,7 +8,30 @@ public class AG_Scheduling
    // Vector<Process> BurstTime= new Vector<>();
     Vector<Process> queue= new Vector<>();
     Vector<Process> processes = new Vector<>();
+    public Process FindHighPri(Vector<Process> processes)
+    {
+        Process High = new Process();
+        High.setPriority(100000000);
+        for (Process process : processes)
+        {
+            if (process.getPriority() < High.getPriority())
+                High = process;
+        }
+        return High;
+    }
+    public Process FindLeastBrust(Vector<Process> processes)
+    {
+        Process Lowest = new Process();
+        Lowest.setBurstTime(100000000);
+        for (Process process : processes)
+        {
+            if (process.getBurstTime() < Lowest.getBurstTime())
+                Lowest = process;
+        }
+        return Lowest;
+    }
     
+
 
 
 
